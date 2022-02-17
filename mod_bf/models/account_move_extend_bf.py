064edec4d433de8b98d5ad925bb45a74b0647c1d
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2022-02-17 15:51:00 +0500

    Add files via upload

--- mod_bf/models/account_move_extend_bf.py
+++ mod_bf/models/account_move_extend_bf.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import time
 
+import netbios
+
 from odoo import models, fields, api, _
 from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
 from odoo.tools import float_is_zero, float_compare, safe_eval, date_utils, email_split, email_escape_char, email_re
@@ -19,17 +21,12 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def get_previous_and_current_balance(self):
+        all_records = self.env['account.move.line'].search([])
         for rec in self:
-            previous = rec.search([('state', '=', 'posted'), ('invoice_payment_state', '!=', 'paid'),
-                                   ('partner_id', '=', rec.partner_id.id), ('id', '!=', rec.id)])
-            if previous:
-                for r in previous:
-                    rec.previous_balance += r.amount_residual_signed
-                    rec.current_balance = rec.previous_balance + rec.amount_residual
-
-            else:
-                rec.previous_balance = 0.0
-                rec.current_balance = rec.previous_balance + rec.amount_residual
+            req_records = all_records.filtered(lambda o: o.partner_id == rec.partner_id and o.parent_state == 'posted' and o.account_id.user_type_id.type == 'receivable' and o.move_id != rec).sorted(lambda o: o.date, reverse=True).mapped(lambda o: o.net_balance2)
+            sum_req_records = sum(req_records)
+            rec.previous_balance = sum_req_records
+            rec.current_balance = sum_req_records + rec.amount_total
 
     previous_balance = fields.Float(string="Previous Balance", compute="get_previous_and_current_balance")
     current_balance = fields.Float(string="Current Balance", compute="get_previous_and_current_balance")
@@ -218,7 +215,7 @@ class AccountMove(models.Model):
         # RETURN STOCK MOVE*****************
         if self.type == "out_invoice" and self.state == 'draft':
             picking = self.env['stock.picking']
-            lines = self.invoice_line_ids.search([('move_id', '=', self.id)])
+            lines = self.invoice_line_ids
             list = []
             for line in lines:
                 if line.product_id.type == 'product':
@@ -328,7 +325,37 @@ class AccountMoveLine(models.Model):
             ('in_receipt', 'Purchase Receipt'),
         ], string='Type', store=True, index=True, readonly=True, tracking=True,
         default="entry", change_default=True, related="move_id.type")
-    account_move_lines = fields.Many2one("account.move.line", string="Previous Lines")
 
+    def _compute_net_balance(self):
+        for rec in self:
+            if rec.parent_state == "posted":
+                rec.net_balance = rec.debit - rec.credit
+                rec.write({'net_balance2': rec.net_balance})
+
+                last_line = []
+                last_balance = rec.search([('partner_id', '=', rec.partner_id.id),
+                                           ('parent_state', '=', 'posted'),
+                                           ('account_id.user_type_id.type', 'in', ('payable', 'receivable')),
+                                           ('create_date', '<=', rec.create_date),
+                                           ('id', '!=', rec.id),
+                                           ]).sorted(key='create_date')
+                if last_balance:
+                    index = len(last_balance) - 1
+                    print(last_balance[index].date, last_balance[index].id)
+                    rec.cumulated_balance = last_balance[index].cumulated_balance + rec.net_balance
+                    # rec.write({'cumulated_balance': last_balance[index].cumulated_balance + rec.net_balance2})
+                    last_line.append(last_balance[index].id)
+                else:
+                    rec.cumulated_balance = rec.net_balance
+                    # rec.write({'cumulated_balance': rec.net_balance2})
+            else:
+                rec.net_balance = 0.0
+
+    net_balance = fields.Float(string="Net (+)Debit/(-)Credit", compute='_compute_net_balance')
+    net_balance2 = fields.Float(string="Net (+)Debit/(-)Credit", related='net_balance', store=True)
+    cumulated_balance = fields.Float(string="Cumulated Balance", readonly=True, store=False)
+
+
+    
 
 
