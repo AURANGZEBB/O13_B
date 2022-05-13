62790cd865de107caf214ea040f904f6d10052da
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2022-05-13 21:21:41 +0500

    Add files via upload

---
+++ print_invoice_bf/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, 2020 The Adepts
+# License LGPL-3.0 or later (https://www.theadepts.net).
+
+{
+    "name": "BF Invoice Print",
+    "summary": "BF Invoice Print",
+    "version": "13.0.1.3",
+    "category": "",
+    "website": "https://www.theadepts.net",
+	"description": """
+
+    """,
+	'images':[
+
+	],
+    "author": "The Adepts",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'base',
+        'account',
+        'mod_bf',
+    ],
+    "data": [
+        'report/invoice_template.xml',
+        'report/report.xml',
+    ],
+
+    #'live_test_url': 'https://youtu.be/JX-ntw2ORl8'
+
+}
