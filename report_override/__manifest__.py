398e4bd3abe227e9e6068aa2db2f4c94b2b92257
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2023-05-06 22:37:23 +0500

    Add files via upload

---
+++ report_override/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Report Override",
+    "summary": "Report Override",
+    "author": "ACSONE SA/NV," "Creu Blanca," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/reporting-engine",
+    "category": "Reporting",
+    "version": "13.0.1.0.6",
+    "development_status": "Production/Stable",
+    "license": "AGPL-3",
+    "external_dependencies": {"python": []},
+    "depends": ["base_accounting_kit"],
+    "data": ['reports/report_partner_ledger.xml',],
+    "demo": [],
+    "installable": True,
+}
